ca42068650cb7c88a834577b05e3413149458847
Lsrock <42789764+Lsrock@users.noreply.github.com>
2020-02-13 09:44:29 -1000

    DNF

---
+++ clones.js
@@ -0,0 +1,13 @@
+function hasClones() {
+  const list = [];
+  clone = _.map()
+  return _.contains(list, 'CLONES');
+}
+
+console.log(hasClones(['foo', 'bar bar', 'bazCLONESbaz']));
+
+
+
+//function numClones(){
+//  return _.countBy(list, function(num) {return "CLONES" ?  'Total:';});
+//}
